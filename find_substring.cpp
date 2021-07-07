91a4a0c2350b8f2144b05a3bc8e88b646d7d3148
Mehul-Gupta-2177 <mehul.gupta@tufts.edu>
2021-07-08 00:12:00 +0530

    to_still change using KMp algorithm

--- find_substring.cpp
+++ find_substring.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
 // finds a how many times a substring is there within a given string
-int number_of_time(string str, string substr);
+// and printing its location
+// Optimal O(n) solution = KMP algorithm
+vector<int> number_of_time(string str, string substr);
 int main() {
 
     // sets and gets all the strings
@@ -21,21 +24,30 @@ int main() {
 
 }
 
-int number_of_time(string str, string substr){
+vector<int> number_of_time(string str, string substr){
     
+    // checker 1
+    vector<int> index;
     if(substr.size() > str.size()){
-        return 0;
+        index.push_back(0);
+        return index;
     }
-    
-    bool a = true; 
+    // checker 2
+    if(substr.size() == str.size()){
+        if(substr == str){
+            index.push_back(1);
+            index.push_back(0);
+            return index;
+        }
+    }
 
-    int initial_char_at = 0;
-    while(a){
-        
+    int final_char_index = str.size() - substr.size() - 1;
+    int count = 0;
+    for(int i = 0; i <= final_char_index; i++){
+
     }
 
 
-
-    return 0;
+    return index;
 
 }
